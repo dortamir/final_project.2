275565f90d12796bb6e91c22bfebd3fd54effb78
dortamir <dortamir291@gmail.com>
2024-07-22 15:52:27 +0300

    ring+neck

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,5 +3,6 @@ const usersController = require('../controllers/usersControllers');
 
 router.post('/users/signUp', usersController.signUp);
 router.post('/users/signIn', usersController.signIn);
+router.get('/users/logout', usersController.logout);
 
 module.exports = router;
