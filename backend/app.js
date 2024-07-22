3ca627d17ff6beb48c53412ab16f9a69238b376e
dortamir <dortamir291@gmail.com>
2024-07-22 19:34:46 +0300

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -66,5 +66,113 @@ const createApp = async function () {
       return res.status(500).json({ error: 'Internal Server Error' });
     }
   });
+
+  app.get('/check-login', (req, res) => {
+    if (req.session.user || req.cookies.user) {
+      // User is logged in
+      return res.status(200).json({ loggedIn: true, user: req.session.user || req.cookies.user });
+    } else {
+      // User is not logged in
+      return res.status(200).json({ loggedIn: false });
+    }
+  });
+
+  app.get('/', function (req, res) {
+    // Check if the user is logged in and retrieve the username from the cookies or wherever you store it
+    const loggedIn = req.session.user || req.cookies.user;
+    const username = loggedIn ? loggedIn.username : null;
   
+    // Render the index.ejs template and pass the loggedIn and username variables as data
+    res.render('index', { loggedIn: !!loggedIn, username: username });
+  });
+
+  app.get('/logout', async (req, res) => {
+    try {
+      // Clear the user's session or remove the user's cookie
+      // For example, if you are using Express sessions with cookies:
+      res.clearCookie('user'); // Clear the session cookie
+      res.clearCookie('order');
+
+      req.session.destroy(); // Destroy the session
+
+      // Log a message to the terminal
+      console.log('User logged out successfully.');
+
+      // Redirect the user to the login page or any other desired page
+      res.redirect('/'); // Redirect to the homepage
+    } catch (error) {
+      console.error('Error Logging Out:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+  app.get('/userprofile', (req, res) => {
+    const username = req.cookies.user ? req.cookies.user.username : null; // Retrieve the 'username' cookie value if available
+    res.render('userprofile', { username }); // Pass the 'username' variable to the template
+  });
+  
+  app.get("/Necklaces", async (req, res) => {
+    try {
+      const username = req.cookies.user ? req.cookies.user.username : null;
+      const Necklaces = await Item.find({ type: "Necklace" }).exec();
+      // console.log(fruits); // Log the retrieved Necklaces data
+      res.render("Necklaces", { username, Necklaces });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Failed to retrieve Necklaces" });
+    }
+  });
+
+  app.get("/Rings", async (req, res) => {
+    try {
+      const username = req.cookies.user ? req.cookies.user.username : null;
+      const Rings = await Item.find({ type: "Ring" }).exec();
+      // console.log(Rings); // Log the retrieved fruits data
+      res.render("Rings", { username, Rings });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Failed to retrieve Rings" });
+    }
+  });
+
+  app.get("/Bracelets", async (req, res) => {
+    try {
+      const username = req.cookies.user ? req.cookies.user.username : null;
+      const Bracelets = await Item.find({ type: "Bracelate" }).exec();
+      // console.log(Bracelets); // Log the retrieved fruits data
+      res.render("Bracelets", { username, Bracelets });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Failed to retrieve Bracelets" });
+    }
+  });
+
+  app.get("/earrings", async (req, res) => {
+    try {
+      const username = req.cookies.user ? req.cookies.user.username : null;
+      const earrings = await Item.find({ type: "Earrings" }).exec();
+      
+      res.render("earrings", { username, earrings });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Failed to retrieve earrings" });
+    }
+  });
+
+  app.get("/contact", (req, res) => {
+    res.render("contact");
+  });
+
+  app.get("/up", (req, res) => {
+    res.render("up");
+  });
+
+    app.get("/up", (req, res) => {
+      const username = req.cookies.user ? req.cookies.user.username : null; // Retrieve the 'username' cookie value if available
+      res.render("up", { username });
+    });
+
+
+
+
 }
